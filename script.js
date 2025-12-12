bf955525f178cba78b7459ae0bd5387ae61284fd
lPanares <panareslynnard@gmail.com>
2025-12-12 19:58:18 +0800

    Refactor click event handler for character count

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const txtInput = document.querySelector('#txtInput');
 const btnCount = document.querySelector('#btnCount');
 const txtOutput = document.querySelector('#txtOutput');
 
-btnCount.addEventListener('click', function() {
-    const input = txtInput.value;
-    const trimmed = input.trim();
-    const count = trimmed.length;
-    txtOutput.textContent = count;
+btnCount.addEventListener('click', function(){
+    let input = txtInput.value;
+    txtOutput.textContent = input.trim().length;
 });
